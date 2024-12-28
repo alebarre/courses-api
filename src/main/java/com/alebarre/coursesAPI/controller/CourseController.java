54080db162e4ab0c1704029192a9ea917e646a08
Alexandre Barreto <plotartemail@gmail.com>
2024-12-28 17:42:13 -0300

    Criação controller 'findById'

--- src/main/java/com/alebarre/coursesAPI/controller/CourseController.java
+++ src/main/java/com/alebarre/coursesAPI/controller/CourseController.java
@@ -3,7 +3,13 @@ package com.alebarre.coursesAPI.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +26,19 @@ public class CourseController {
 	@GetMapping
 	public List<Course> list(){
 		return courseRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Course> findById(@PathVariable Long id) {
+		return courseRepository.findById(id)
+				.map(record -> ResponseEntity.ok().body(record))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Course> create(@RequestBody Course course) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(courseRepository.save(course));
 	}
 
 }
