64917cbf7e6e969d20a40ff7f6c556b244472494
alebarre <plotartemail@gmail.com>
2025-01-30 23:17:57 -0300

    Configuração docker, mysql e application.properties para varios ambientes

--- src/main/java/com/alebarre/coursesAPI/CoursesApiApplication.java
+++ src/main/java/com/alebarre/coursesAPI/CoursesApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import com.alebarre.coursesAPI.enums.Category;
 import com.alebarre.coursesAPI.enums.Status;
@@ -19,6 +20,7 @@ public class CoursesApiApplication {
 	}
 
 	@Bean
+	@Profile("dev")
 	CommandLineRunner initDataBase(CourseRepository courseRepository) {
 		return argas -> {
 			courseRepository.deleteAll();
